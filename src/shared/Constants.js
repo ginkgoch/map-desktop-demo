fac9b458d33298ab4690c889b49496296a8df7cc
Ginkgoch <ginkgoch@outlook.com>
2020-02-17 15:35:07 +0800

    3: Add spatial ops demos.

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,6 +1,6 @@
 import L from "leaflet";
 
-let defaultStyle = {
+const defaultStyle = {
     radius: 8,
     fillColor: "#ff7800",
     color: "#000",
@@ -8,10 +8,15 @@ let defaultStyle = {
     opacity: 1,
     fillOpacity: 0.4
 };
+const defaultOSMAttribution = `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`;
+const defaultOSMUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 export default {
-    OSM_ATTRIBUTION: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`,
-    OSM_SERVICE_URL: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    OSM_ATTRIBUTION: defaultOSMAttribution,
+    OSM_SERVICE_URL: defaultOSMUrl,
+    DEFAULT_OSM_OPTIONS: {
+        attribution: defaultOSMAttribution
+    },
     DEFAULT_GEO_JSON_STYLE: defaultStyle,
     DEFAULT_GEO_JSON_OPTIONS: {
         style: defaultStyle,
