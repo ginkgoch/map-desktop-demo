bedfb552e66b4a55dada576c1a58f0db310fa4a0
Ginkgoch <ginkgoch@outlook.com>
2020-02-17 11:57:51 +0800

    8: Refactor demo list.

---
+++ src/components/examples/geoms/index.js
@@ -0,0 +1,11 @@
+import Geometries from '@/components/examples/geoms/Geometries'
+import SpecialPolygons from '@/components/examples/geoms/SpecialPolygons'
+import GeocentricLine from '@/components/examples/geoms/GeocentricLine'
+
+let demos = [  
+    Geometries,
+    SpecialPolygons,
+    GeocentricLine
+]
+
+export default demos;
--- src/components/examples/index.js
+++ src/components/examples/index.js
@@ -1,9 +1,7 @@
 import HelloMap from '@/components/examples/HelloMap'
 import LoadShapefile from '@/components/examples/LoadShapefile'
-import Geometries from '@/components/examples/geoms/Geometries'
-import SpecialPolygons from '@/components/examples/geoms/SpecialPolygons'
-import GeocentricLine from '@/components/examples/geoms/GeocentricLine'
+import GeomDemos from './geoms';
 
 export default [
-    HelloMap, LoadShapefile, Geometries, SpecialPolygons, GeocentricLine
+    HelloMap, LoadShapefile, ...GeomDemos
 ];
