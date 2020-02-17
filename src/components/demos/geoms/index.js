0a5e4aebc3cbb090ba7ea08a37aa579372b79f01
Ginkgoch <ginkgoch@outlook.com>
2020-02-17 11:59:17 +0800

    8: Rename examples to demos.

---
+++ src/components/demos/geoms/index.js
@@ -0,0 +1,11 @@
+import Geometries from '@/components/demos/geoms/Geometries'
+import SpecialPolygons from '@/components/demos/geoms/SpecialPolygons'
+import GeocentricLine from '@/components/demos/geoms/GeocentricLine'
+
+let demos = [  
+    Geometries,
+    SpecialPolygons,
+    GeocentricLine
+]
+
+export default demos;
--- src/components/examples/index.js
+++
@@ -1,7 +0,0 @@
-import HelloMap from '@/components/examples/HelloMap'
-import LoadShapefile from '@/components/examples/LoadShapefile'
-import GeomDemos from './geoms';
-
-export default [
-    HelloMap, LoadShapefile, ...GeomDemos
-];
