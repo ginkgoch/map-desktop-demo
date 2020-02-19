7b2e7b78c0fc7f476be58511e892967d4c884082
Ginkgoch <ginkgoch@outlook.com>
2020-02-19 19:39:11 +0800

    Add CSV and GeoJSON demos.

---
+++ src/components/demos/sources/GeoJSON.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div id="mapContainer"></div>
+    <section>
+      <button class="btn btn-outline-success" @click="addFeatureLayer">Add a FeatureLayer with Delimited File</button>
+      <button class="btn btn-outline-success" @click="renderProperties">Get Delimited File Properties</button>
+    </section>
+    <section class="table-container">
+      <table class="table table-sm" v-if="tableSource.length > 0">
+        <thead>
+          <tr>
+            <th v-for="(cell, k) of tableSource[0]" :key="k">{{ cell }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, rkey) of tableSource.slice(1)" :key="rkey">
+            <td v-for="(cell, ckey) of row" :key="ckey" class="small">{{ cell }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  </div>
+</template>
+
+<script>
+import L from "leaflet";
+import Constants from "../../../shared/Constants";
+import DemoUtils from '../../../shared/DemoUtils';
+import { FeatureGridLayer } from 'ginkgoch-leaflet-extensions';
+import { FeatureLayer, GeneralStyle, GeoJSONFeatureSource, Projection, ViewportUtils } from 'ginkgoch-map';
+
+export default {
+  name: "geojson-source",
+  title: "GeoJSON Source",
+  desc: `Use GeoJSON source to query and render on map.`,
+  detail: `GeoJSON source is used to fetch features from a specific delimited file on your local machine; it exposes a set of API to query and render on map.`,
+  data() {
+    return {
+      tableSource: [],
+      comments: ''
+    };
+  },
+  mounted() {
+    this.map = L.map("mapContainer").setView([0, 0], 2);
+    L.tileLayer(Constants.OSM_SERVICE_URL, Constants.DEFAULT_OSM_OPTIONS).addTo(this.map);
+  },
+  methods: {
+    async addFeatureLayer() {
+      if (this.gridLayer !== undefined) {
+        return;
+      }
+
+      let featureSource = new GeoJSONFeatureSource(DemoUtils.resolveExtraResourcePath('airports.json'));
+      featureSource.projection = new Projection('WGS84', 'EPSG:3857');
+
+      let featureLayer = new FeatureLayer(featureSource);
+      featureLayer.styles.push(new GeneralStyle('#fff7bc', '#d95f0e', 1, 6));
+
+      this.gridLayer = new FeatureGridLayer();
+      this.gridLayer.pushLayer(featureLayer);
+      this.gridLayer.addTo(this.map);
+
+      await featureLayer.open();
+      let envelope = await featureLayer.envelope();
+
+      let { x, y } = this.map.getSize();
+      let viewportInfo = ViewportUtils.getInitViewport(envelope, x, y);
+      this.map.setView([viewportInfo.lat, viewportInfo.lng], viewportInfo.zoom);
+    },
+    async renderProperties() {
+      let featureSource = new GeoJSONFeatureSource(DemoUtils.resolveExtraResourcePath('airports.json'));
+      await featureSource.open();
+
+      let features = null;
+      features = await featureSource.features();
+
+      this.tableSource = featuresToTableSource(features);
+    }
+  }
+};
+
+function featuresToTableSource(features) {
+  let tableSource = [];
+  if (features.length > 0) {
+    tableSource.push(features[0].properties.keys());
+  }
+
+  features.forEach(f => tableSource.push(f.properties.values()));
+
+  return tableSource;
+}
+</script>
+
+<style>
+.table-container {
+  max-height: 400px;
+  overflow-y: auto;
+}
+</style>
--- src/components/demos/sources/index.js
+++ src/components/demos/sources/index.js
@@ -1,8 +1,10 @@
 import Memory from '@/components/demos/sources/Memory'
 import Shapefile from '@/components/demos/sources/Shapefile'
+import DelimitedFile from '@/components/demos/sources/DelimitedFile'
+import GeoJSON from '@/components/demos/sources/GeoJSON'
 
 let demos = [  
-    Memory, Shapefile
+    Memory, Shapefile, DelimitedFile, GeoJSON
 ]
 
 export default demos;
