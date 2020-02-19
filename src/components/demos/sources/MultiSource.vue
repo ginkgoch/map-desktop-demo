ddaee432e5bdf491c118e4006c19bdc7ce5e2c69
Ginkgoch <ginkgoch@outlook.com>
2020-02-19 21:34:01 +0800

    Add multi source demo.

---
+++ src/components/demos/sources/MultiSource.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div id="mapContainer"></div>
+    <section>
+      <button class="btn btn-outline-success" @click="addFeatureLayer">Add a FeatureLayer with Multiple Source</button>
+    </section>
+  </div>
+</template>
+
+<script>
+import L from "leaflet";
+import Constants from "../../../shared/Constants";
+import DemoUtils from '../../../shared/DemoUtils';
+import { FeatureGridLayer } from 'ginkgoch-leaflet-extensions';
+import { MultiSourceFeatureLayer, GeoJSONFeatureSource, Projection, ViewportUtils, ShapefileFeatureSource, FillStyle, PointStyle } from 'ginkgoch-map';
+
+export default {
+  name: "multi-source",
+  title: "Multi Source",
+  desc: `Use Multi source to query and render on map.`,
+  detail: `Multi source is used to fetch features from a specific delimited file on your local machine; it exposes a set of API to query and render on map.`,
+  data() {
+    return {
+      tableSource: [],
+      comments: ''
+    };
+  },
+  mounted() {
+    this.map = L.map("mapContainer").setView([0, 0], 2);
+    L.tileLayer(Constants.OSM_SERVICE_URL, Constants.DEFAULT_OSM_OPTIONS).addTo(this.map);
+  },
+  methods: {
+    async addFeatureLayer() {
+      if (this.gridLayer !== undefined) {
+        return;
+      }
+
+      let shpFeatureSource = new ShapefileFeatureSource(DemoUtils.resolveExtraResourcePath('cntry02.shp'));
+      let jsonFeatureSource = new GeoJSONFeatureSource(DemoUtils.resolveExtraResourcePath('airports.json'));
+      jsonFeatureSource.projection = new Projection('WGS84', 'EPSG:3857');
+
+      let featureLayer = new MultiSourceFeatureLayer([shpFeatureSource, jsonFeatureSource]);
+      featureLayer.styles.push(new FillStyle('rgba(254,232,200,0.4)', '#d95f0e', 1));
+      featureLayer.styles.push(new PointStyle('#e34a33', 'black', 1, 2));
+
+      this.gridLayer = new FeatureGridLayer();
+      this.gridLayer.pushLayer(featureLayer);
+      this.gridLayer.addTo(this.map);
+
+      await featureLayer.open();
+      let envelope = await featureLayer.envelope();
+
+      let { x, y } = this.map.getSize();
+      let viewportInfo = ViewportUtils.getInitViewport(envelope, x, y);
+      this.map.setView([viewportInfo.lat, viewportInfo.lng], viewportInfo.zoom);
+    }
+  }
+};
+</script>
--- src/components/demos/sources/index.js
+++ src/components/demos/sources/index.js
@@ -2,9 +2,10 @@ import Memory from '@/components/demos/sources/Memory'
 import Shapefile from '@/components/demos/sources/Shapefile'
 import DelimitedFile from '@/components/demos/sources/DelimitedFile'
 import GeoJSON from '@/components/demos/sources/GeoJSON'
+import MultiSource from '@/components/demos/sources/MultiSource'
 
 let demos = [  
-    Memory, Shapefile, DelimitedFile, GeoJSON
+    Memory, Shapefile, DelimitedFile, GeoJSON, MultiSource
 ]
 
 export default demos;
